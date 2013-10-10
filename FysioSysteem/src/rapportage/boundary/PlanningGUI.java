d76f33f82cb3dd1535f1ca4c8c6b0a541c2ae969
Rogier Welten <rogier_w@live.nl>
2013-10-10 12:25:32 +0200

    PlanningGUI aangepast

--- FysioSysteem/src/rapportage/boundary/PlanningGUI.java
+++ FysioSysteem/src/rapportage/boundary/PlanningGUI.java
@@ -27,6 +27,7 @@ public class PlanningGUI extends javax.swing.JFrame {
     public PlanningGUI(BehandelingManager manager) {
         initComponents();
         control = new PlanningControl((DefaultTableModel) overzichtTabel.getModel(), manager);
+        this.setVisible(true);
     }
 
     /**
@@ -198,35 +199,7 @@ public class PlanningGUI extends javax.swing.JFrame {
         }
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PlanningGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new PlanningGUI().setVisible(true);
-            }
-        });
-    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton datumButton;
     private javax.swing.JTextField einddatumTextField;
