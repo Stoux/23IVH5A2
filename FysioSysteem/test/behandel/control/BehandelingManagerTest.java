c2d18366bbcd79380a3733f10a0e39b9cad63ebb
Intrexion <ndemie@gmail.com>
2013-10-15 21:33:15 +0200

    JUnit behandeling uitgebreid

--- FysioSysteem/test/behandel/control/BehandelingManagerTest.java
+++ FysioSysteem/test/behandel/control/BehandelingManagerTest.java
@@ -6,7 +6,10 @@ package behandel.control;
 
 import behandel.entity.Behandeling;
 import data.control.DataController;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import javax.swing.table.DefaultTableModel;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,6 +18,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import praktijk.control.TherapeutManager;
+import praktijk.entity.Therapeut;
 
 /**
  *
@@ -27,8 +31,6 @@ public class BehandelingManagerTest {
     
     public BehandelingManagerTest() {
         DataController dc = new DataController();
-        TherapeutManager tm = new TherapeutManager(dc);
-        BehandelingManager bm = new BehandelingManager(dc, tm);
         dm = new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null, null, null, null},
@@ -40,6 +42,7 @@ public class BehandelingManagerTest {
                 "ID", "Patiënt BSN", "Fysiotherapeut BSN", "Behandelcode", "Datum", "Begin tijd", "Eind tijd", "Status", "Opmerkingen"
             }
         );
+        bm = new BehandelingManager(dc, tm);
     }
     
     @BeforeClass
@@ -58,6 +61,25 @@ public class BehandelingManagerTest {
     public void tearDown() {
     }
 
+    
+    
+    /*
+     * Test of testParseDateString method
+     */
+    @Test
+    public void testParseDateString() {
+        System.out.println("getBehandeling");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date datum = new Date();
+        try {
+            datum = sdf.parse("14/10/2013 15:00");
+        } catch (ParseException ex) {
+            System.out.println("Fout testParseDateString() " + ex.toString());  
+        }
+        Date parse = bm.parseDateString("14/10/2013 15:00");
+        Assert.assertEquals(datum, parse);  
+    }
+    
     /**
      * Test of getBehandeling method.
      */
@@ -105,25 +127,50 @@ public class BehandelingManagerTest {
         Assert.assertNotSame(voor, na);
     }
     /**
-     * Test of isFysiotherapeutBeschikbaar method
+     * Test of isFysiotherapeutBeschikbaar method (positief)
      */
     @Test
-    public void testIsFysiotherapeutBeschikbaar() {
-        System.out.println("isFysiotherapeutBeschikbaar");
+    public void testIsFysiotherapeutBeschikbaarPositief() {
+        System.out.println("isFysiotherapeutBeschikbaarPositief");
+        bm.maakBehandeling(123456789, "AB1", 987654321, bm.parseDateString("10/12/2013 15:30"), bm.parseDateString("10/12/2013 16:30"), Behandeling.Status.Ingepland, "Opmerking test");
+        boolean expResult = true;
+        boolean result = bm.isFysiotherapeutBeschikbaar(987654321, bm.parseDateString("10/12/2013 14:30"), bm.parseDateString("10/12/2013 15:00"));
+        Assert.assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of isFysiotherapeutBeschikbaar method (negatief)
+     */
+    @Test
+    public void testIsFysiotherapeutBeschikbaarNegatief() {
+        System.out.println("isFysiotherapeutBeschikbaarNegatief");
         bm.maakBehandeling(123456789, "AB1", 987654321, bm.parseDateString("10/12/2013 14:15"), bm.parseDateString("10/12/2013 15:30"), Behandeling.Status.Ingepland, "Opmerking test");
         boolean expResult = false;
         boolean result = bm.isFysiotherapeutBeschikbaar(987654321, bm.parseDateString("10/12/2013 14:30"), bm.parseDateString("10/12/2013 15:00"));
         Assert.assertEquals(expResult, result);
     }
+    
     /*
-     * Test of isBestaandeTherapeut method
+     * Test of isBestaandeTherapeut method (positief)
      */
     @Test
-    public void testIsBestaandeTherapeut() {
-        System.out.println("isBestaandeTherapeut");
-        boolean expResult = false;
+    public void testIsBestaandeTherapeutPositief() {
+        System.out.println("isBestaandeTherapeutPositief");
+        tm.voegToe(new Therapeut("Kees", "van", "Voort", bm.parseDateString("19/08/1978"), Therapeut.Geslacht.Mannelijk, 456789123, "5964AB", "103", "010390349", 8964745));
+        boolean expResult = true;
         boolean result = bm.isBestaandeTherapeut(456789123); 
         Assert.assertEquals(expResult, result);   
     }
     
+    /*
+     * Test of isBestaandeTherapeut method
+     */
+    @Test
+    public void testIsBestaandeTherapeutNegatief() {
+        System.out.println("isBestaandeTherapeutNegatief");
+        tm.voegToe(new Therapeut("Henk", "de", "Koe", bm.parseDateString("07/10/1963"), Therapeut.Geslacht.Mannelijk, 111222333, "2651CD", "2", "01016541", 8964745));
+        boolean expResult = false;
+        boolean result = bm.isBestaandeTherapeut(456789123); 
+        Assert.assertEquals(expResult, result);   
+    }
 }
